e0fb03f1cf7737acddfc55ee68c2e53467ff57ed
Victoria Moore <victoriamoore563@gmail.com>
2019-10-04 17:36:28 -0700

    beginin stages of psu-code, tho idk if I want more

---
+++ index.js
@@ -0,0 +1,12 @@
+const STORE = 
+    //I want to try to add all repetative content in here. Meaning
+    //my project section 
+
+hiddenWindow => {
+    //this will be where I add the functionality that will
+    //allow me to hover over an area and the content will change.
+    //will either be the bio are or the project area. Though
+    //I might do the image for the project also but keep the area
+    //that displays the description for it completeyly visible
+}
+
