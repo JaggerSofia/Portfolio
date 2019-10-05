4d3b6db95bd07bfe05a962f4dcecd6f1e35e084f
Victoria Moore <victoriamoore563@gmail.com>
2019-10-05 12:04:00 -0700

    decided not to use STORE

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const STORE = 
-    //I want to try to add all repetative content in here. Meaning
-    //my project section 
-
 hiddenWindow => {
     //this will be where I add the functionality that will
     //allow me to hover over an area and the content will change.
