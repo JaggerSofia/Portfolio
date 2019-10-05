0dd2bd8fb64faa8d5ff32ef7f3ed652747fbb2a3
Victoria Moore <victoriamoore563@gmail.com>
2019-10-05 12:33:14 -0700

    updated fonts

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-hiddenWindow => {
+function hiddenWindow() {
     //this will be where I add the functionality that will
     //allow me to hover over an area and the content will change.
     //will either be the bio are or the project area. Though
     //I might do the image for the project also but keep the area
     //that displays the description for it completeyly visible
+    $('#bio').mouseenter(function(){
+        $('.about').hide();
+        $('.description').show();
+    })
+
+    $('#bio').mouseleave(function(){
+        $('.about').show();
+        $('.description').hide();
+    })
 }
 
+hiddenWindow();
