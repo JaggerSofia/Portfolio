d9c9441eb1bb7dc14ff6145925a18811eff4a302
Victoria Moore <victoriamoore563@gmail.com>
2019-10-05 14:44:30 -0700

    took out borders

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// function hiddenWindow() {
-//     //this will be where I add the functionality that will
-//     //allow me to hover over an area and the content will change.
-//     //will either be the bio are or the project area. Though
-//     //I might do the image for the project also but keep the area
-//     //that displays the description for it completeyly visible
-//     console.log('hiddenWindow')
-//     $('#bio').mouseenter(function(){
-//         $('.about').hide();
-//         $('.description').show();
-//     })
+function hiddenWindow() {
+    //this will be where I add the functionality that will
+    //allow me to hover over an area and the content will change.
+    //will either be the bio are or the project area. Though
+    //I might do the image for the project also but keep the area
+    //that displays the description for it completeyly visible
+    console.log('hiddenWindow')
+    $('#bio').mouseenter(function(){
+        $('.about').hide();
+        $('.description').show();
+    })
 
-//     $('#bio').mouseleave(function(){
-//         $('.about').show();
-//         $('.description').hide();
-//     })
-// }
+    $('#bio').mouseleave(function(){
+        $('.about').show();
+        $('.description').hide();
+    })
+}
 
-// hiddenWindow();
+hiddenWindow();
