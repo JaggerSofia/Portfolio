8ce78db56648095f8b7bbb7e8355464476511315
Victoria Moore <victoriamoore563@gmail.com>
2019-10-06 20:21:23 -0700

    moved pseudocode & changed init listener on line 1

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-$('document').ready( function() {
-    //this will be where I add the functionality that will
-    //allow me to hover over an area and the content will change.
-    //will either be the bio are or the project area. Though
-    //I might do the image for the project also but keep the area
-    //that displays the description for it completeyly visible
-    console.log('hiddenWindow')
+$(function() {
     $('.interests').mouseenter(function(){
         $('.about-me').hide();
         $('.description-me').show();
@@ -25,3 +19,9 @@ $('document').ready( function() {
         $('.description-pro').hide();
     })
 });
+
+    //this will be where I add the functionality that will
+    //allow me to hover over an area and the content will change.
+    //will either be the bio are or the project area. Though
+    //I might do the image for the project also but keep the area
+    //that displays the description for it completeyly visible
